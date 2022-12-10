33a298bc2425678cbb110789b4abc2b51fd4c957
neoreign <newreignabdulakeem@gmail.com>
2022-12-09 20:04:43 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*Enjoying the alx program*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
