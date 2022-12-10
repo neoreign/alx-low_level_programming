e541753e8a1e869983f5187661f164d97af19632
neoreign <newreignabdulakeem@gmail.com>
2022-12-09 21:09:29 -0800

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
-}  
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
