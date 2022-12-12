c3e256e9d2f672799e005eca2eba22c58c159012
neoreign <newreignabdulakeem@gmail.com>
2022-12-12 11:02:02 -0800

    confusion

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int d;
+for (d = 0; d < 90; d++)
+{
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+}
+{
+if (d != 89)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
