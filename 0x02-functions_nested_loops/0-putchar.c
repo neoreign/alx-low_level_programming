fb5fb6cce1ceea406f5ad795282939f5fcc41fd5
neoreign <newreignabdulakeem@gmail.com>
2022-12-14 03:17:21 -0800

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
-#include "_putchar.h"
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by neoreign
+ * Return: 0
  */
 int main(void)
 {
-char *sh = "_putchar";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h 
 #define main_h 
   
-char _putchar(char); 
+int _putchar(char); 
 void print_alphabet(void); 
 void print_alphabet_x10(void); 
 int _islower(int c); 
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n); 
 void print_times_table(int n); 
   
-#endif /* HOLBERTON_H */
+#endif
