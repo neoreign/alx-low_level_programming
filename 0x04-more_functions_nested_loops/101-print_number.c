ecb04e8738d6511e6e6369c273db18736b971db9
neoreign <newreignabdulakeem@gmail.com>
2022-12-16 16:49:52 -0800

    tough

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_number - a functiom that prints an integer
+ * @n: input
+ * Return: input integer
+ */
+void print_number(int n)
+{
+int i;
+while (i > 0)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+return (0);
+}
